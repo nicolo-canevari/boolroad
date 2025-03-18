1c7f90c80c2884d86a9472b46ab8b505e7b4b5c4
nicolo_canevari <nico.canevari@outlook.it>
2025-03-18 17:45:29 +0100

    refuso

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,17 @@
 import TripList from "../components/TripList";
 
-
+// Definisce il componente HomePage
 const HomePage = () => {
+
     return (
+
         <div>
+            {/* Mostra il componente TripList all'interno della homepage */}
             <TripList />
         </div>
+
     );
+
 }
 
 export default HomePage;
